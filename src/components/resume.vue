16843f0ea32729c28e51001dda4488a2354c5794
mvcuccaro <mvcuccaro@noreply.github.com>
2018-07-22 01:24:56 -0400

    pull git repos from github api

--- src/components/resume.vue
+++ src/components/resume.vue
@@ -1,19 +1,35 @@
 <template>
 	<div>
 		<resume-header v-bind:objective="resume.objective"></resume-header>
-		<div class="card">
-			<div class="text-right mb-0 pb-0" style="border-bottom: 1px dotted #C0C0C0;"><i class="fa fa-sort mr-3" v-on:click="sortData('skills', 'name')"></i></div>
-			<div class="card-body text-primary bg-white">
-				<div class="row bg-white"> 
-					<resume-skill-item v-for="item in resume.skills" v-bind:skillitem="item" v-bind:key="item.name" class="mb-2"></resume-skill-item>
+		<div class="row">
+			<div class="col-sm-9">
+				<div class="card">
+					<div class="text-right mb-0 pb-0" style="border-bottom: 1px dotted #C0C0C0;"><i class="fa fa-sort mr-3" v-on:click="sortData('skills', 'name')"></i></div>
+					<div class="card-body text-primary bg-white">
+						<div class="row bg-white"> 
+							<resume-skill-item v-for="item in resume.skills" v-bind:skillitem="item" v-bind:key="item.name" class="mb-2"></resume-skill-item>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="text-right mb-0 pb-0" style="border-bottom: 1px dotted #C0C0C0;"><i class="fa fa-sort mr-3" v-on:click="sortData('experiences', 'title')"></i></div>
+					<div class="card-body text-primary bg-white">
+						<div class="row bg-white"> 
+							<resume-experience-item v-for="item in resume.experiences" v-bind:experienceitem="item" v-bind:key="item.company" class="mb-2"></resume-experience-item>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="card">
-			<div class="text-right mb-0 pb-0" style="border-bottom: 1px dotted #C0C0C0;"><i class="fa fa-sort mr-3" v-on:click="sortData('experiences', 'title')"></i></div>
-			<div class="card-body text-primary bg-white">
-				<div class="row bg-white"> 
-					<resume-experience-item v-for="item in resume.experiences" v-bind:experienceitem="item" v-bind:key="item.company" class="mb-2"></resume-experience-item>
+			<div class="col-sm-3 ml-0 pl-0" id="repos_container">
+				<div class="card">
+					<div class="card-header">My Github Repos</div>
+					<div class="card_body" id="repos_list_body">
+						<ul>
+							<li v-for="item in my_repos">
+								<a :href="item.svn_url" :title="item.description">{{ item.name }}</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -34,6 +50,7 @@ export default {
   	data () {
     	return {
     		resume: {},
+    		my_repos: [],
     		sort_directions:{skills: true, experience:true}
     	}
   	},
@@ -44,6 +61,12 @@ export default {
 			this.resume = r.data;
 			console.log(this.resume.skills);
 		})
+	},
+	getRepos(){
+		this.$axios.get(this.$config.github_repos_url)
+		.then(r => {
+			this.my_repos = r.data;
+		});
 	},
 	sortData(arg_data_type, arg_sort_column){
 		this.resume[arg_data_type].sort((a,b) => {
@@ -59,7 +82,8 @@ export default {
 	}	
   },
   mounted(){
-  	this.getResume()
+  	this.getResume(),
+  	this.getRepos()
   }
 } 
 </script>
@@ -67,5 +91,8 @@ export default {
 <style scoped>
 #header {
 	background-color:#C0C0FF;
+}
+#repos_list_body {
+	font-size:14px;
 }
 </style>
--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,9 @@
 const config = {
 	title:'michaelcuccaro.com',
 	api_url: 'https://api.michaelcuccaro.com',
-	profile_image: "https://avatars0.githubusercontent.com/u/9444185?s=400&u=c2e2bb71a5cdac0687ddf98715017ea34e773dea&v=4"
+	github_repos_url: 'https://api.github.com/users/mvcuccaro/repos',
+	profile_image: "https://avatars0.githubusercontent.com/u/9444185?s=400&u=c2e2bb71a5cdac0687ddf98715017ea34e773dea&v=4",
+
 }
 
 module.exports = config
