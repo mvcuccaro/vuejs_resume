6ef62cb83dcda91f80f2fc1893132e839231e94c
mvcuccaro <mvcuccaro@noreply.github.com>
2018-07-21 22:35:20 -0400

    remove https since its handled by the proxy

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const express 	= require('express');
 const fs 		= require('fs');
 const https 	= require('https');
@@ -6,14 +8,21 @@ const resume 	= require('./data/resume.js');
 
 let private_key = fs.readFileSync('./keys/server.key', 'utf8');
 let certificate = fs.readFileSync('./keys/server.crt', 'utf8');
+
+/**
+* https handled by proxy
 let credentials = {
 	key: private_key,
 	cert: certificate
 };
+*/
 
 const app = new express();
-let https_server = https.createServer(credentials, app);
+//let https_server = https.createServer(credentials, app); https handled mby proxy
+
 let http_server  = http.createServer(app);
+
+app.use('/dist', express.static('../dist'));
 
 
 app.use(function(req, res, next) { 
@@ -22,7 +31,7 @@ app.use(function(req, res, next) {
 
 app.get('/', (req, res) => {
 	console.log(req.url);
-	const rs = fs.createReadStream('./index.html');
+	const rs = fs.createReadStream('../index.html');
 	rs.pipe(res);
 })
 
@@ -34,9 +43,12 @@ app.get('/data/resume', (req, res) => {
 	res.end(JSON.stringify(resume));
 })
 
+/*
+* https handled by proxy
 https_server.listen(443, () => {
 	console.log('listening on 443');
 })
+*/
 
 http_server.listen(8075, () => {
 	console.log('listening on 8075');
