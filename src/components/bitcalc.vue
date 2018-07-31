1d3c4a21d9ddc49c70fdf2cf8d288669b3913180
mvcuccaro <mvcuccaro@noreply.github.com>
2018-07-31 00:41:43 -0400

    porting angular-crud-bits to a bitcalc for vuejs

---
+++ src/components/bitcalc.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<div>
+				<div><input type="checkbox" :id="'bc_cb_' + _uid + '1'" value="1" v-model="bits"></div>
+				<div><input type="checkbox" :id="'bc_cb_' + _uid + '2'" value="2" v-model="bits"></div>
+				<div><input type="checkbox" :id="'bc_cb_' + _uid + '4'" value="4" v-model="bits"></div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default{
+	props:['p_total'],
+	data(){
+		return {
+			bits:[],
+			total:0
+		}
+	},
+	computed:{
+	},
+	watch: {
+		bits: function(val){
+			this.total = this.bits.reduce( (a,b) => {
+				return parseInt(a) + parseInt(b);
+			}, 0)
+			this.$emit('totalChanged', this.total);
+		}
+	},
+	mounted(){
+		this.total = this.p_total;
+	}
+}
+</script>
--- src/components/dev_tools.vue
+++ src/components/dev_tools.vue
@@ -3,20 +3,30 @@
 		<div class="card-header bg-primary">{{ name }}</div>
 			<div class="card-body bg-secondary">
 				<md5er></md5er>
+				<bitcalc @totalChanged="bitcalcTotalChanged($event)" :total="bitcalc_total"></bitcalc>
+				<div>bitcalc total: {{ bitcalc_total }}</div>
 			</div>
 		</div>
 </template>
 
 <script>
 import md5er from './md5er.vue';
+import bitcalc from './bitcalc.vue';
 
 export default {
 	components: {
-		md5er: md5er
+		md5er: md5er,
+		bitcalc: bitcalc
 	},
 	data(){
 		return {
-			name: 'dev tools'
+			name: 'dev tools',
+			bitcalc_total: 0
+		}
+	},
+	methods:{
+		bitcalcTotalChanged(val){
+			this.bitcalc_total = val;
 		}
 	}
 }
