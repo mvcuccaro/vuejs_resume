b424d399b53163e44166ee31d7067b30530208b2
mvcuccaro <mvcuccaro@noreply.github.com>
2018-08-05 23:05:04 -0400

    add unfinished calc component so webpack can compile

---
+++ src/components/calculator.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<div class="card col-6 p-0">
+			<div class="card-header">{{ name }}</div>
+			<div v-for="index in 10" :key="index">{{ index }}</div>
+			</div>
+		</div>
+	</div>
+
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			name: 'Calculator'
+		}
+	}
+}
+</script>
